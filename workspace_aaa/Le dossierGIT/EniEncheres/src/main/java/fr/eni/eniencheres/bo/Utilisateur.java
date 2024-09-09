d1acc4d151414a3daece54709ba59117bd4bd05c
Hugo <hugo.covu@gmail.com>
2024-09-09 11:45:46 +0200

    essai

---
+++ workspace_aaa/Le dossierGIT/EniEncheres/src/main/java/fr/eni/eniencheres/bo/Utilisateur.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package fr.eni.eniencheres.bo;
+
+/**
+ * Classe en charge de 
+ * @projet : EniEncheres - V1.0
+ * @author : hcovu2024
+ * @since: 9 sept. 2024 - 10:28:47
+ */
+public class Utilisateur {
+	
+	private Integer noUtilisateur;
+	private String pseudo;
+	private String nom;
+	private String email;
+
+}
